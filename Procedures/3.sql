b4d26751bb15ae8a9c82515957f3d44d9c992b81
Javier Pimentel <jpimentel7@users.noreply.github.com>
2015-04-29 19:15:55 -0700

    Update 3.sql

--- Procedures/3.sql
+++ Procedures/3.sql
@@ -3,17 +3,17 @@
 */
 CREATE PROCEDURE retrieveClasses
 AS
-SELECT [dbo].[classe].[id]
+SELECT [dbo].[class].[id]
 	  ,[title]
       ,[description]
       ,[duration]
       ,[difficulty_level]
 	  ,[first_name]
 	  ,[last_name]
-  FROM [dbo].[classe]
+  FROM [dbo].[class]
 
   JOIN [dbo].[presenter]
-	ON [dbo].[classe].[presenter_id] = [dbo].[presenter].[id]
+	ON [dbo].[class].[presenter_id] = [dbo].[presenter].[id]
   JOIN [dbo].[attendent]
 	on [dbo].[presenter].[attendent_id] = [dbo].[attendent].[id];
 GO
